37c9414e4b5d73f98bbe190f564d22698058c8cb
Hugo van den Berg <hugo@tbdwebdesign.nl>
2016-08-23 01:23:25 +0200

    Updated timezones, states in Code Book

---
+++ doc/STATE.R
@@ -0,0 +1,18 @@
+STATE <- data.frame(
+    from = c(
+        state.abb,
+        "GU",
+        "DC",
+        "PR",
+        "AS",
+        "MH"
+        ),
+    to = c(
+        state.name,
+        "Guam",
+        "District of Columbia",
+        "Puerto Rico",
+        "American Samoa",
+        "Marshall Islands"
+        )
+)
